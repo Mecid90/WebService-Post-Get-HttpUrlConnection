b8235d7ac7673b534dcd4d61378d0d445cf0e2cc
Mecid90 <mecidm90@gmail.com>
2023-01-25 14:12:38 +0400

    initial commit

--- src/main/java/ExceptionHandler/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ExceptionHandler;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/controller/HttpConnectionPost.java
+++ src/main/java/controller/HttpConnectionPost.java
@@ -1,51 +1,55 @@
 package controller;
 
 
+
 import org.apache.commons.codec.binary.Base64;
-import service.PayInvoiceService;
-import service.PayInvoiceServiceImpl;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class HttpConnection {
+
+public class HttpConnectionPost {
 
     public static void main(String[] args) {
 
-        InputStreamReader inputStream = null;
-        OutputStream outputStream = null;
-        BufferedReader bufferedReader = null;
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-        HttpURLConnection httpURLConnection;
-
-
-        URL url;
-
         {
+            InputStreamReader inputStream = null;
+            OutputStream outputStream = null;
+            BufferedReader bufferedReader = null;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            HttpURLConnection httpURLConnection;
+
+
+            URL url;
             try {
-
-                PayInvoiceService payInvoiceService = new PayInvoiceServiceImpl();
-
-                byte[] out = payInvoiceService.toString().getBytes();
-
-
                 url = new URL("https://test-link.yigim.az/payInvoice");
                 httpURLConnection = (HttpURLConnection) url.openConnection();
                 String userCredentials = "testlinkusername:testlinkpassword";
-                String auth ="Basic " + new String(new Base64().encode(userCredentials.getBytes()));
+                String auth = new String(new Base64().encode(userCredentials.getBytes()));
+
 
                 httpURLConnection.setRequestProperty("X-Signature",auth);
+                httpURLConnection.setRequestProperty("Content-Type", "application/json");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+
+                String jsonBody = "{\"id\": 1,\"amount\": 15,\"currency\": 9446,\"transactionId\": \"00000003\",\"agent\":\"\",\"PBMB\":\"\"}";
+
+                byte[] out = jsonBody.getBytes();
 
                 httpURLConnection.setRequestMethod("POST");
+
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
 
-                httpURLConnection.setConnectTimeout(200);
-                httpURLConnection.setReadTimeout(200);
+
+
+                httpURLConnection.setConnectTimeout(2000);
+                httpURLConnection.setReadTimeout(2000);
+
+
 
                 httpURLConnection.connect();
 
@@ -58,6 +62,8 @@ public class HttpConnection {
 
                 int status = httpURLConnection.getResponseCode();
 
+
+
                 if (status==HttpURLConnection.HTTP_OK){
                     inputStream = new InputStreamReader(httpURLConnection.getInputStream());
                     bufferedReader = new BufferedReader(inputStream);
@@ -65,10 +71,13 @@ public class HttpConnection {
                     while ((line=bufferedReader.readLine()) != null){
                         stringBuilder.append(line);
                     }
+                    System.out.println(line);
                 }
-                System.out.println(status);
 
+
+                System.out.println(status);
                 System.out.println(stringBuilder);
+
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
